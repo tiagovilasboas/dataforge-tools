0448ee33d4649eeef720e02e16898b74e4790471
Tiago Montanha <tcarvalhovb@gmail.com>
2025-07-03 06:32:41 -0300

    feat: add project configuration and build setup

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'vendor-react';
+            if (id.includes('zod')) return 'vendor-zod';
+            if (id.includes('lucide-react')) return 'vendor-lucide';
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
+})
